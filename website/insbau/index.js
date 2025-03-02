d909e4811c7e704dd7d732fd0b291ccb9b1a690c
mestokaya1830 <mesto1830@outllok.com>
2025-03-02 17:18:57 +0100

    ccc

--- website/insbau/index.js
+++ website/insbau/index.js
@@ -8,5 +8,15 @@ document.querySelectorAll('.links').forEach(item => {
       item2.style.color = "#fff"
     })
     item.style.color = '#fddf85'
+    document.querySelector('.mobil-navigation').style.display = "none"
   })
+})
+document.querySelectorAll('.mobil-links').forEach(item => {
+  item.addEventListener('click', () => {
+    document.querySelector('.mobil-navigation').classList.toggle('mobil-menu-handle')
+  })
+})
+
+document.querySelector('.mobil-navigation-icon').addEventListener('click', (e) => {
+  document.querySelector('.mobil-navigation').classList.toggle('mobil-menu-handle')
 })
