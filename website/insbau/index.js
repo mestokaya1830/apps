ea67ce428404ebf7932cc4dc185c4fbc78ae81e6
mestokaya1830 <mesto1830@outllok.com>
2025-03-03 22:27:04 +0100

    xxs

--- website/insbau/index.js
+++ website/insbau/index.js
@@ -20,3 +20,41 @@ document.querySelectorAll('.mobil-links').forEach(item => {
 document.querySelector('.mobil-navigation-icon').addEventListener('click', (e) => {
   document.querySelector('.mobil-navigation').classList.toggle('mobil-menu-handle')
 })
+
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const subject = document.getElementById('subject')
+const message = document.getElementById('message')
+let errorUsername =  document.getElementById('error-username')
+let errorSubject =  document.getElementById('error-subject')
+let errorEmail =  document.getElementById('error-email')
+let errorMessage =  document.getElementById('error-message')
+
+let pattern = {
+  username: /^.{3,50}[a-z0-9]$/,
+  email: /([^ ]+)@([^ ]+)\.([a-z]{2,3})(\.[a-z]{2,3})?/,
+  subject: /^.{5,50}\w$/,
+  message:/^.{10,50}\w$/
+}
+
+document.querySelectorAll('.inputs').forEach(item => {
+  item.addEventListener('keyup', (e) => {
+    e.target.nextElementSibling.innerText = ''
+  })
+})
+
+document.getElementById('submit').addEventListener('click' , (e) => {
+  e.preventDefault()
+  if(!username.value.match(pattern.username)){
+    username.focus()
+    errorUsername.innerText = 'Username must be more 2 letter!'
+  } else if(!email.value.match(pattern.email)){
+    errorEmail.innerText = 'Email is invalid format!'
+  } else if(!subject.value.match(pattern.subject)){
+    errorSubject.innerText = 'Subject must be more 5 letter!'
+  } else if(!message.value.match(pattern.message)){
+    errorMessage.innerText = 'Message must be more 10 letter!'
+  } else {
+    document.getElementById("email-form").reset();
+  }
+})
