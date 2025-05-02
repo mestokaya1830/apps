0f58a920d7001cc26010c25905728270d7c7c8f9
mestokaya1830 <mesto1830@outllok.com>
2025-05-02 18:27:33 +0200

    cdcd

---
+++ website/mesweb/index.js
@@ -0,0 +1,159 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".links")[0].style.setProperty("--afterWidth", "100%");
+  // if(document.querySelector('.banner-overlay').offsetWidth > 1024){
+  //   document.querySelectorAll(".links")[0].style.color = "#0eaa93ff";
+  //   document.querySelectorAll(".links")[0].style.setProperty("--afterWidth", "100%");
+  // }
+  // document.getElementById("loading-container").style.display = "none";
+  // document.getElementById("main-container").style.display = "block";
+
+  window.onscroll = () => {
+    if (
+      document.body.scrollTop > 500 ||
+      document.documentElement.scrollTop > 500
+    ) {
+      // document.getElementById("go-top").style.display = "block";
+    } else {
+      // document.getElementById("go-top").style.display = "none";
+    }
+    // if(document.querySelector('.banner-overlay').offsetWidth > 1024){
+    //   const sections = document.querySelectorAll(".sections");
+    //   let current = "";
+    //   sections.forEach((item) => {
+    //     const sectionTop = item.offsetTop;
+    //     if (scrollY >= sectionTop - 60) {
+    //       current = item.getAttribute("id");
+    //     }
+    //   });
+  
+    //   document.querySelectorAll(".links").forEach((item) => {
+    //     item.style.setProperty("--afterWidth", 0);
+    //     item.style.color = "darkgray";
+    //     if (item.getAttribute("id") == current + "-link") {
+    //       item.style.color = "#80af8e";
+    //       item.style.setProperty("--afterWidth", "100%");
+    //     }
+    //   });
+    // }
+  };
+
+  // document.getElementById("go-top").addEventListener("click", (e) => {
+  //   window.scrollTo(0, 0);
+  //   document.querySelectorAll(".links").forEach((item2) => {
+  //     item2.style.color = "darkgray";
+  //   });
+  //   document.getElementById("startseite-link").style.color = "#80af8e";
+
+  //   setTimeout(() => {
+  //     document
+  //       .querySelectorAll(".links")[0]
+  //       .style.setProperty("--afterWidth", "100%");
+  //   }, 100);
+  // });
+
+  // if(document.querySelector('.banner-overlay').offsetWidth > 1024){
+  //   document.querySelectorAll(".links").forEach((item) => {
+  //     item.addEventListener("click", () => {
+  //       document.querySelector(".mobil-navigation").style.display = "none";
+  //       setTimeout(() => {
+  //         history.replaceState("", document.title,
+  //           window.location.origin +
+  //             window.location.pathname +
+  //             window.location.search
+  //         );
+  //       }, 100);
+  //     });
+  //   });
+  // }
+
+  // document.querySelectorAll(".mobil-links").forEach((item) => {
+  //   item.addEventListener("click", () => {
+  //     document
+  //       .querySelector(".mobil-navigation")
+  //       .classList.toggle("mobil-menu-handle");
+  //   });
+  // });
+
+  // document
+  //   .querySelector(".mobil-navigation-icon")
+  //   .addEventListener("click", (e) => {
+  //     document
+  //       .querySelector(".mobil-navigation")
+  //       .classList.toggle("mobil-menu-handle");
+  //   });
+
+  const username = document.getElementById("username");
+  const email = document.getElementById("email");
+  const subject = document.getElementById("subject");
+  const message = document.getElementById("message");
+  let errorUsername = document.getElementById("error-username");
+  let errorSubject = document.getElementById("error-subject");
+  let errorEmail = document.getElementById("error-email");
+  let errorMessage = document.getElementById("error-message");
+
+  let pattern = {
+    username: /^.{3,50}[a-z0-9]$/,
+    email: /([^ ]+)@([^ ]+)\.([a-z]{2,3})(\.[a-z]{2,3})?/,
+    subject: /^.{5,50}\w$/,
+    message: /^.{10,50}\w$/,
+  };
+
+  document.querySelectorAll(".inputs").forEach((item) => {
+    item.addEventListener("keyup", (e) => {
+      e.target.nextElementSibling.innerText = "";
+    });
+  });
+
+  const sr = ScrollReveal({
+    distance: "60px",
+    duration: 2500,
+    delay: 400,
+    reset: true,
+  });
+
+  sr.reveal("#banner-img", { delay: 300, origin: "right" });
+  sr.reveal("#banner-left", { delay: 300, origin: "left" });
+
+  // if ('serviceWorker' in navigator) {
+  //   navigator.serviceWorker.register('sw.js').then(res => {
+  //       console.log(res)
+  //   }).catch((err) => {
+  //       console.log(err)
+  //   })
+  // }
+
+  document.getElementById("submit").addEventListener("click", (e) => {
+    e.preventDefault();
+    if (!username.value.match(pattern.username)) {
+      username.focus();
+      errorUsername.innerText =
+        "Der Benutzername muss mehr als 2 Buchstaben haben!";
+    } else if (!email.value.match(pattern.email)) {
+      errorEmail.innerText = "E-Mail hat ein ungültiges Format!";
+    } else if (!subject.value.match(pattern.subject)) {
+      errorSubject.innerText = "Der Betreff muss mehr als 5 Buchstaben haben!";
+    } else if (!message.value.match(pattern.message)) {
+      errorMessage.innerText =
+        "Die Nachricht muss mehr als 10 Buchstaben haben!";
+    } else {
+      emailjs.init({
+        publicKey: "pvpLz1XCQL_clu1Cr",
+      });
+      const data = {
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+      };
+      emailjs.send("service_haih1te", "template_qxyo28p", data).then(
+        (response) => {
+          alert("Ihre Nachricht wurde erfolgreich gesendet");
+        },
+        (error) => {
+          console.log("FAILED...", error);
+        }
+      );
+      document.getElementById("email-form").reset();
+    }
+  });
+  
+});
